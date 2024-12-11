c4af5a46e503f6d2594f49a44c4006ff8290c3c0
Daniel CJ <jekson998@blaster.co.id>
2024-12-11 20:51:33 +0700

    Initial commit

---
+++ lib/src/util/alt_provider_reader.dart
@@ -0,0 +1,6 @@
+// ignore_for_file: prefer_typing_uninitialized_variables
+
+// Used for emergency riverpod state reader. Cannot be used for watching nor listening.
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+late WidgetRef ref_;
